cd544bb375a0d8757cf97a321ef0d45dd7ec3eec
yogevbd <yogev132@gmail.com>
2019-09-20 04:05:06 +0300

    Fix valid labels input

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function run() {
     const validLabels = core.getInput('valid-labels', {required: true}).split(',');
     core.debug(`Verified PR match valid labels: ${validLabels}`);
 
-    const labels = github.context!.payload!.pull_request!.labels.split(',');
+    const labels = github.context!.payload!.pull_request!.labels;
     console.log(`PR labels: ${labels}`);
 
-    if (!labels.some(l => validLabels.includes(l))) {
+    if (!labels.split(',').some(l => validLabels.includes(l))) {
       core.setFailed(`Please select one of the required labels for this PR: ${validLabels}`);
     }
   } catch (error) {
