e66d448f1e64d849b24798f10660a4cdf789a57a
yogevbd <yogev132@gmail.com>
2019-09-20 10:49:12 +0300

    print github context

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function run() {
     core.debug(`Verified PR match valid labels: ${validLabels}`);
 
     const labels = github.context!.payload!.pull_request!.labels;
-    console.log(`PR labels: ${JSON.stringify(labels)}`);
+    console.log(`PR labels: ${JSON.stringify(github.context)}`);
 
     if (!labels.some(l => validLabels.includes(l.name))) {
       core.setFailed(`Please select one of the required labels for this PR: ${validLabels}`);
